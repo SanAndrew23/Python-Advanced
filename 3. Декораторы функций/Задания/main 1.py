7814df1f493b27c710e656708321a7af910e79cf
AndreyRudenko <rudenkoandrey2304@gmail.com>
2026-06-10 15:06:52 +0300

    Решение задач

--- 3. Декораторы функций/Задания/main 1.py
+++ 3. Декораторы функций/Задания/main 1.py
@@ -1,9 +1,12 @@
+from functools import wraps
+
+
 def trace(func):
-    def wrapper(*args):
-        print(func.__name__)
-        print(args)
-        result = func(*args)
-        return result
+    @wraps(func)
+    def wrapper(*args, **kwargs):
+        result = func(*args, **kwargs)
+        args_string = [f'{k} = {str(v)}' for k, v in kwargs.items()]
+        print(f'{func.__name__}({", ".join(args_string)}) = {result}')
     return wrapper
 
 @trace
--- 3. Декораторы функций/Задания/main 2.py
+++ 3. Декораторы функций/Задания/main 2.py
@@ -1,11 +1,20 @@
 def once(func):
     def wrapper(a):
         b = func(a)
-        func(a)
+        wrapper.count += 1
+        if wrapper.count <= 1:
+            return func(a)
+        else:
+            return b
+    wrapper.count = 0
     return wrapper
 
 @once
 def summarise(a):
     return a + 1
 
+print(summarise(1))
+print(summarise(1))
+print(summarise(1))
+print(summarise(1))
 print(summarise(1))
