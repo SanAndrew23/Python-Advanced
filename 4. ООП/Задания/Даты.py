e869377efb1a406de6fb9f69b1c3215493e812c6
AndreyRudenko <rudenkoandrey2304@gmail.com>
2026-07-08 14:08:38 +0300

    Решение задач

---
+++ 4. ООП/Задания/Даты.py
@@ -0,0 +1,37 @@
+class Date:
+    def __init__(self, day=1, month=1, year=1970):
+        self.day = day
+        self.month = month
+        self.year = year
+
+    def set_from_str(self, date):
+        date = date.split('.')
+        self.day = date[0]
+        self.month = date[1]
+        self.year = date[2]
+
+    def print(self):
+        print(f'{self.day}.{self.month}.{self.year}')
+
+    def add_day(self):
+        lst = [1, 3, 5, 7, 8, 9, 11]
+        is_vis = (self.year % 100 != 0 and self.year % 4 == 0) or (self.year % 400 == 0)
+        if self.month == 2:
+            if is_vis:
+                max_d = 29
+            else:
+                max_d = 28
+        elif self.month in lst:
+            max_d = 30
+        else:
+            max_d = 31
+        self.day += 1
+        if self.day > max_d:
+            self.day = 1
+            self.month += 1
+            if self.month > 12:
+                self.month = 1
+                self.year += 1
+date = Date()
+date.set_from_str('14.06.2025')
+date.print()
