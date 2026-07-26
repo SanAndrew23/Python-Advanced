5df7aaf3d22d27934661ff38fd86e02d1b1569f4
AndreyRudenko <rudenkoandrey2304@gmail.com>
2026-07-26 17:45:02 +0300

    Решение задач

--- 4. ООП/Задания/Класс Matrix.py
+++ 4. ООП/Задания/Класс Matrix.py
@@ -1,30 +1,82 @@
+from pkgutil import resolve_name
+
+
 class Matrix:
-    def __init__(self, rows=None, cols=None):
-        if isinstance(rows, int):
+    def __init__(self, rows=0, cols=0, *, data=None):
+        if data is None:
             self.rows = rows
             self.cols = cols
             self.matrix = [[0 for j in range(self.cols)] for i in range(self.rows)]
         else:
-            self.matrix = rows
-            self.cols = len(rows[0])
-            self.rows = len(rows)
+            self.matrix = data
+            self.rows = len(data)
+            self.cols = len(data[0])
+
     def __getitem__(self, index):
         return self.matrix[index]
+
     def __add__(self, other):
+        if self.rows == other.rows and self.cols == other.cols:
+            result = [[0 for j in range(self.cols)] for i in range(self.rows)]
+            for i in range(self.rows):
+                for j in range(self.cols):
+                    result[i][j] = self.matrix[i][j] + other.matrix[i][j]
+            return Matrix(data=result)
+        else:
+            raise ValueError('Размеры матриц не совпадают.')
 
     def __sub__(self, other):
+        if self.rows == other.rows and self.cols == other.cols:
+            result = [[0 for j in range(self.cols)] for i in range(self.rows)]
+            for i in range(self.rows):
+                for j in range(self.cols):
+                    result[i][j] = self.matrix[i][j] - other.matrix[i][j]
+            return Matrix(data=result)
+        else:
+            raise ValueError('Размеры матриц не совпадают.')
 
     def __mul__(self, other):
+        result = [[0 for j in range(self.cols)] for i in range(self.rows)]
+        if isinstance(other, (int, float)):
+            for i in range(self.rows):
+                for j in range(self.cols):
+                    result[i][j] = self.matrix[i][j] * other
+            return Matrix(data=result)
+        elif self.cols == other.rows:
+            result = [[0 for j in range(other.cols)] for i in range(self.rows)]
+            for i in range(self.rows):
+                for j in range(other.cols):
+                    for k in range(self.cols):
+                        result[i][j] += self.matrix[i][k] * other.matrix[k][j]
+            return Matrix(data=result)
+        else:
+            raise ValueError('Размеры матриц не совпадают.')
 
     def transpose(self):
+        result = [[0 for j in range(self.rows)] for i in range(self.cols)]
+        for i in range(self.rows):
+            for j in range(self.cols):
+                result[j][i] = matrix1[i][j]
+        return Matrix(data=result)
 
     def __str__(self):
-        s = ''
-        for i in range(0, self.rows - 1):
-            for j in range(0, self.cols - 1):
-                s += str(self.matrix[i][j]) + ' '
-        return s
+        lines = []
+        m_s = len(str(max(x for row in self.matrix for x in row)))
+        for row in self.matrix:
+            row_s = ''.join(f'{i : {m_s + 1}}' for i in row)
+            lines.append(row_s)
+        return '\n'.join(lines)
 
 
-matrix = Matrix(5, 2)
-print(matrix)
+matrix1 = Matrix(data=[[1, 2], [4, 5]])
+matrix2 = Matrix(data=[[7, 8], [10, 11]])
+try:
+    print(matrix1 + matrix2)
+    print()
+    print(matrix1 - matrix2)
+    print()
+    print(matrix1 * matrix2)
+    print()
+    print(matrix1.transpose())
+except ValueError:
+    print(f'Ошибка: размеры матриц не совпадают')
