25773dfc08b900c6c417d721f2bc1addb8a3d8c8
Андрей Боев <a.boev.it@gmail.com>
2026-08-19 17:43:54 +0300

    UPD: 4. ООП > Money - редактирование

--- 4. ООП/Задания/ATM system.py
+++ 4. ООП/Задания/ATM system.py
@@ -1,45 +1,28 @@
-class Money():
-    def __init__(self, currency='', balance=0, owner='', *, f=None):
+class Money:
+    def __init__(self, owner="", *, f=None):
+        self.cash = {0.01 : 0,  0.05 : 0,
+                     0.10 : 0,  0.50 : 0,
+                     1 : 0,     2 : 0,
+                     5 : 0,     10 : 0,
+                     50 : 0,    100 : 0,
+                     200 : 0,   500 : 0,
+                     1000 : 0,  2000 : 0,
+                     5000 : 0}
         if f:
-            self.currency = f['currency']
-            self.__balance = f['balance']
-        else:
-            self.owner = owner
-            self.__balance = balance
-            self.currency = currency
-    def convert(self, currency):
-        sp = {
-            'USD': {'balance' : 0.0},
-            'RUB': {'balance': 0.0},
-            'EUR': {'balance': 0.0},
-        }
-        c = 0
-        for i in sp:
-            if i == self.currency:
-                sp[i]['balance'] = self.__balance
-                flag = c
-            c += 1
-        if self.currency == 'USD':
-            sp['EUR']['balance'] = round(0.86 * self.__balance, 2)
-            sp['RUB']['balance'] = round(84.54 * self.__balance, 2)
-        elif self.currency == 'EUR':
-            sp['RUB']['balance'] = round(1.157 * self.__balance, 2)
-            sp['USD']['balance'] = round(97.51 * self.__balance, 2)
-        else:
-            sp['USD']['balance'] = round(0.01187 * self.__balance, 2)
-            sp['EUR']['balance'] = round(0.01026 * self.__balance, 2)
+            pass
 
-        self.sp = sp
-        return sp[currency]['balance']
+    @property
+    def balance(self):
+       pass
 
-    def __add__(self, currency, balance):
-        slovar = {'balance': balance, 'currency': currency}
-        result = self.__balance + Money(f=slovar).convert(self.currency)
-        return result
+    def __add__(self, other):
+        '''
+        Метод складывает другой "кошелёк" и возвращает новый объект с новой суммой
+        '''
+        pass
 
     def __str__(self):
-        return self
+        return f'{f"{self.balance:,.2f}".replace(',', ' ')} ₽'
 
-money = Money('RUB',1500, 'Andrew')
-print(money.convert('USD'))
-print(money + Money(f={'currency': 'RUB', 'balance': 1500}))
+
+money = Money("Andrew")
